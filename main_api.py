5a5803387548785ea5878dc728a09928a28b9dac
Roman Parisi <109306308+romanprs03@users.noreply.github.com>
2026-04-21 17:06:43 -0300

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from scraping import Instagram, Tiktok, Youtube, X
+from scraping import Instagram, Tiktok
 
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
 
 @app.get("/instagram/{username}")
 def instagram(username: str, type: str = "clean"):
@@ -10,5 +14,5 @@ def instagram(username: str, type: str = "clean"):
 
 @app.get("/tiktok/{username}")
 def tiktok(username: str, type: str = "clean"):
-    tt = Tiktok()  # <- con minúscula
+    tt = Tiktok()
     return tt.get(f"@{username}", type=type)
